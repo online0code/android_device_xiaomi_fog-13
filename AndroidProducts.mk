dae50a0fa5464f756f471ea36ba3bde7cf6ae3fc
online0code <151095682+online0code@users.noreply.github.com>
2025-02-20 04:17:58 +0000

    CarbonRom Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/komodo_fog.mk
+    $(LOCAL_DIR)/carbon_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    komodo_fog-user \
-    komodo_fog-userdebug \
-    komodo_fog-eng
+    carbon_fog-user \
+    carbon_fog-userdebug \
+    carbon_fog-eng
---
+++ carbon_fog.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit from fog device
+$(call inherit-product, device/xiaomi/fog/device.mk)
+
+PRODUCT_NAME := carbon_fog
+PRODUCT_DEVICE := fog
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi 10C
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Redmi/fog_global/fog:12/SKQ1.211103.001/V13.0.5.0.SGEMIXM:user/release-keys
